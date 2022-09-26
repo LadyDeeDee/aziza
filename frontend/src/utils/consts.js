5e87a34cf9b486b9764946b988210d2fbd28c9de
LadyDeeDee <dilyafruzik@gmail.com>
2022-09-26 11:21:57 +0200

    bug fixed

--- frontend/src/utils/consts.js
+++
@@ -1,7 +0,0 @@
-export const ADMIN_ROUTE = '/admin'
-export const LOGIN_ROUTE = '/login'
-export const REGISTRATION_ROUTE = '/registration'
-export const SHOP_ROUTE = '/shop'
-export const BASKET_ROUTE = '/basket'
-export const PRODUCT_ROUTE = '/product'
-//export const CONTACTFORM_ROUTE = '/contactform'
